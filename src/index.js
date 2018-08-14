ac176832de8dea66ac4d1e71ea141969e0572180
Andrew Rogala <adrogala87@gmail.com>
2018-08-14 10:55:09 -0400

    add basic react-router-dom navigation between search and home page

--- src/index.js
+++ src/index.js
@@ -1,6 +1,11 @@
-import React from 'react'
-import ReactDOM from 'react-dom'
-import App from './App'
-import './index.css'
-
-ReactDOM.render(<App />, document.getElementById('root'))
+import React from 'react';
+import ReactDOM from 'react-dom';
+import { BrowserRouter } from 'react-router-dom';
+import App from './App';
+import './index.css';
+//set up browser router on the app to keep your UI in sync with the URL
+//this allows the use of Route and Link in the app.js file
+ReactDOM.render(
+	<BrowserRouter><App /></BrowserRouter>,
+	document.getElementById('root')
+);
